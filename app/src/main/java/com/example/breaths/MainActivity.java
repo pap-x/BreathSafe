481633b91fddc942400bcbac3c248dfafc2377ef
pap-x <chrspapadop@gmail.com>
2021-05-19 21:03:34 +0300

    Forecast values from API, working settings button

--- app/src/main/java/com/example/breaths/MainActivity.java
+++ app/src/main/java/com/example/breaths/MainActivity.java
@@ -8,14 +8,22 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,20 +32,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getData("40.674972", "22.895322");
+
+        final ImageButton button = findViewById(R.id.settingsButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, Settings.class);
+                //myIntent.putExtra("key", value); //Optional parameters
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 
+    //API call to get data
     public void getData(String lat, String lon) {
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://api.openweathermap.org/data/2.5/air_pollution?lat="+lat+"&lon="+lon+"&appid="; //2bcdd94a20ae1c5acd2f35b063bb3a0f";
+        String forecast_url = "https://api.openweathermap.org/data/2.5/air_pollution/forecast?lat="+lat+"&lon="+lon+"&appid=";  //2bcdd94a20ae1c5acd2f35b063bb3a0f";
 
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-            new Response.Listener<String>() {
+        //*****************GET THE CURRENT VALUES***********************
+        // Request a JSON response for the current values from the provided URL.
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+            new Response.Listener<JSONObject>() {
                 @Override
-                public void onResponse(String response) {
-                    // Display the response string.
-                    Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                public void onResponse(JSONObject response) {
+                    try {
+                        JSONObject details = response.getJSONArray("list").getJSONObject(0);
+                        String aqi = details.getJSONObject("main").getString("aqi");
+                        JSONObject pollutants = details.getJSONObject("components");
+                        // Display the response string.
+                        Toast.makeText(MainActivity.this, aqi + " " + pollutants.toString(), Toast.LENGTH_SHORT).show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
             }, new Response.ErrorListener() {
                 @Override
@@ -50,12 +77,42 @@ public class MainActivity extends AppCompatActivity {
 
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
-    }
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
+
+        //***********************GET THE FORECAST VALUES**************************
+        // Request a JSON response for the current values from the provided URL.
+        JsonObjectRequest stringRequestF = new JsonObjectRequest(Request.Method.GET, forecast_url, null,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        JSONObject details1 = response.getJSONArray("list").getJSONObject(132);
+                        String aqi1 = details1.getJSONObject("main").getString("aqi");
+                        JSONObject details2 = response.getJSONArray("list").getJSONObject(156);
+                        String aqi2 = details2.getJSONObject("main").getString("aqi");
+                        JSONObject details3 = response.getJSONArray("list").getJSONObject(180);
+                        String aqi3 = details3.getJSONObject("main").getString("aqi");
+                        JSONObject details4 = response.getJSONArray("list").getJSONObject(204);
+                        String aqi4 = details4.getJSONObject("main").getString("aqi");
+                        JSONObject details5 = response.getJSONArray("list").getJSONObject(228);
+                        String aqi5 = details5.getJSONObject("main").getString("aqi");
+                        // Display the response string.
+                        //Toast.makeText(MainActivity.this, aqi + " " + pollutants.toString(), Toast.LENGTH_SHORT).show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(MainActivity.this, "An error occurred!", Toast.LENGTH_SHORT).show();
+                    Log.i("error", String.valueOf(error));
+                }
+            }
+        );
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequestF);
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
