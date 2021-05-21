bfb84fbd5517e40f619714800900ed134990cf48
pap-x <chrspapadop@gmail.com>
2021-05-21 18:38:54 +0300

    Dropdown for conditions, save button functionality

--- app/src/main/java/com/example/breaths/Settings.java
+++ app/src/main/java/com/example/breaths/Settings.java
@@ -3,6 +3,12 @@ package com.example.breaths;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        //Show dropdown for conditions
+        Spinner spinner = (Spinner) findViewById(R.id.condition_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.conditions_array, R.layout.conditions_spinner);
+        // Specify the layout to use when the list of choices appears
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        EditText editName  = (EditText) findViewById(R.id.name_edittext);
+
+        final Button button = findViewById(R.id.save_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses save button
+                String name = editName.getText().toString();
+                Long conditionId = spinner.getSelectedItemId();
+
+                Toast.makeText(Settings.this, name + " " + conditionId.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
